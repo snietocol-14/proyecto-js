b522b4350717e645896b1297429b95cd14597749
snietocol-14 <snietocol@gmail.com>
2026-07-23 17:02:16 -0500

    configuración de la variable del puerto

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from "express"
+import "dotenv/config"
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (_, res) => {
     res.send("Hola , estamos aprendiendo express con la ficha 3407184");
 });
 
 app.listen(port, () => {
-    console.log( `Servidor en funcionamiento en el puerto:  `);
+    console.log(`Servidor en funcionamiento en el puerto ${port}`);
 }); 
