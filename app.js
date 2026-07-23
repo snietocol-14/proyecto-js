90f823f3b3140f092f043253fb7987c315879033
snietocol-14 <snietocol@gmail.com>
2026-07-23 16:10:24 -0500

    creación de app e implementar el método ES en el app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (_, res) => {
+    res.send("Hola , estamos aprendiendo express con la ficha 3407184");
+});
+
+app.listen(port, () => {
+    console.log( `Servidor en funcionamiento en el puerto:  `);
+}); 
