fe4b6cbf8717b3b93b1cd505fa837f8146e66a2e
snietocol-14 <snietocol@gmail.com>
2026-07-30 13:40:04 -0500

    comentar líneas hechas por otro usuario

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ app.get("/", (_, res) => {
 app.listen(port, () => {
     console.log(`Servidor en funcionamiento en el puerto ${port}`);
 }); 
+
+app.get("/saludo/:nombre", (req,res) => {
+            const minombre = req.params. nombre
+            res.send ("mostrar sms")
+    });
+
+//1. productos /: categoria
+//2. productos /: categoria /: Id
+//3. libros /: isbn
+
+app.get("/producto/:categoria", (req,res) => {
+            const minombre = req.params. categoria
+            res.send ("mostrar sms")
+        });
+
+app.get("/producto/:categoria", (req,res) => {
+            const minombre = req.params. id
+            res.send ("mostrar sms")
+        });
+
+app.get("/libro/:isbn", (req,res) => {
+            const minombre = req.params. isbn
+            res.send ("mostrar sms")
+        });
