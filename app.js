1c4934a925a1bb55fcab327c458609c34bc011c8
yeimipadilla085-debug <yeimipadilla085@gmail.com>
2026-08-06 16:45:56 -0500

    config de puerto y endpoint nuevo con commonjs

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (_, res) => {
     res.send("Hola , estamos aprendiendo express con la ficha 3407184");
 });
 
 app.listen(port, () => {
-    console.log( `Servidor en funcionamiento en el puerto:  `);
+    console.log( `Servidor en funcionamiento en el puerto: ${port} `);
 }); 
+
+const { datosUsuario, programa } = require('./datos'); 
+
+app.get('/usuario', (req, res) => {
+    res.json({ 
+        usuario: datosUsuario, 
+        programa: programa 
+    });
+});
---
+++ datos.js
@@ -0,0 +1,19 @@
+// datos.js
+const telefonos = ['12345', '67890', '54321'];
+
+const datosUsuario = {
+    nombre: 'Juan',
+    apellido: 'Pérez',
+    telefono: telefonos
+};
+
+const programa = {
+    nombre: 'Análisis y Desarrollo de Software',
+    tipo: 'Tecnólogo',
+    ficha: '3407184'
+};
+
+module.exports = {
+    datosUsuario,
+    programa
+};   
